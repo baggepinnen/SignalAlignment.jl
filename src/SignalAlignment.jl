81402cb37228d713cfbf041a14edf1cfd7770c78
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-01-31 17:27:53 +0100

    initial code

--- src/SignalAlignment.jl
+++ src/SignalAlignment.jl
@@ -1,5 +1,136 @@
 module SignalAlignment
+using Statistics
+using DSP
+using DynamicAxisWarping
+using DynamicAxisWarping.SlidingDistancesBase
 
-# Write your package code here.
+export Indices, Signals, get_output
+export Delay, XcorrDelay, DTWDelay, compute_delay
+export Warp
+export align_signals, compute_aligning_indices, get_alignment_signals
+
+abstract type AbstractOutput end
+
+Base.@kwdef struct Indices <: AbstractOutput
+    crop::Bool = true
+end
+
+Base.@kwdef struct Signals <: AbstractOutput
+end
+
+function get_output(inds, signals, output::Indices)
+    inds
+end
+
+function get_output(inds, signals, output::Signals)
+    [s[!,i] for (s,i) in zip(signals,inds)]
+end
+
+
+## Methods
+abstract type AbstractMethod end
+
+
+## Delay method
+abstract type AbstractDelayMethod end
+struct XcorrDelay <: AbstractDelayMethod end
+struct DTWDelay <: AbstractDelayMethod end
+
+Base.@kwdef struct Delay <: AbstractMethod
+    delay_method = DTWDelay()
+    master::Int
+end
+
+
+function compute_delay(method::DTWDelay, s1, s2)
+    d,i1,i2 = dtw(s1, s2)
+    round(Int, median(i1-i2))
+end
+
+function compute_delay(method::XcorrDelay, s1, s2)
+    DSP.finddelay(s1, s2)
+end
+
+
+
+function compute_aligning_indices(s, method::Delay)
+    master = method.master
+    inds = [1:lastlength(s) for s in s]
+    # find delays to align with master
+    d = map(s) do si
+        si === s[master] && (return 0)
+        compute_delay(method.delay_method, s[master], si)
+    end
+
+    # find left and right (virtual) zero padding
+    lp = maximum(d)
+    rp = maximum(length(s[master]) .- (length.(s) .+ d))
+    
+    # New window length
+    wl = length(inds[master]) - lp - rp
+    
+    # trim individual index sets to fit into new master window
+    for i in eachindex(inds)
+        start = max(1, 1+lp-d[i])
+        stop = min(length(s[i]),start+wl-1)
+        inds[i] = start : stop
+    end
+    @assert all(length.(inds) .== length(inds[1]))
+    inds
+end
+
+
+## Warp method
+Base.@kwdef struct Warp{T} <: AbstractMethod
+    warp_method::T = DTW
+    master::Int
+end
+
+function compute_aligning_indices(s, method::Warp{<:DTW}) # TODO, maybe the inner method has options
+    master = method.master
+    d = method.warp_method
+    # find delays to align with master
+    inds = map(s) do si
+        si === s[master] && (return collect(1:lastlength(s[master])))
+        _,i1,i2 = distpath(d, si, s[master])
+        i1
+    end
+    inds
+end
+
+# function compute_aligning_indices(s, method::Warp{<:GDTW}) # TODO, maybe the inner method has options
+#     master = method.master
+#     # find delays to align with master
+#     inds = map(s) do si
+#         y = LinearInterpolation(s[master])
+#         si === s[master] && (return y)
+#         x = LinearInterpolation(si)
+#         d,i1,i2 = gdtw(x, y)
+#         i1
+#     end
+#     @assert all(length.(inds) .== length(inds[1])) # TODO: figure this out
+
+#     inds
+# end
+
+function get_alignment_signals(signals::AbstractVector{<:AbstractArray}, ::Nothing)
+    signals
+end
+
+function get_alignment_signals(signals, by::Function)
+    by.(signals)
+end
+
+function get_alignment_signals(signals::AbstractVector{<:AbstractArray}, by::Union{Integer, AbstractVector})
+    [s[by, :] for s in signals]
+end
+
+## Main algorithm
+
+function align_signals(signals, method; by=nothing, output = Indices())
+    s = get_alignment_signals(signals, by)
+    inds = compute_aligning_indices(s, method)
+    get_output(inds, signals, output)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,114 @@ using SignalAlignment
 using Test
 
 @testset "SignalAlignment.jl" begin
-    # Write your tests here.
+signals = [sin.((0:0.01:6pi) .+ rand()) for _ in 1:50]
+signals_unequal = [sin.((0:0.01:(6pi + rand())) .+ rand()) for _ in 1:50]
+signals_short = [sin.((0:0.01:(4pi + rand())) .+ rand()) for _ in 1:10]
+signals_mv = [randn(2,10) for _ in 1:5]
+
+
+@testset "outputs" begin
+    @info "Testing outputs"
+
+    output = Indices()
+    inds = fill(1:10, length(signals))
+    @test get_output(inds, signals, output) == inds
+
+    output = Signals()
+    sa = get_output(inds, signals, output)
+    @test sa == [s[i] for (s,i) in zip(signals, inds)]
+
+    sa = get_output(inds[1:length(signals_mv)], signals_mv, output)
+    @test sa[1] == signals_mv[1][:,inds[1]]
+end
+
+@testset "by" begin
+    @info "Testing by"
+    @test get_alignment_signals(signals, nothing) == signals
+    @test get_alignment_signals(signals, identity) == signals
+    
+end
+
+@testset "Methods" begin
+    @info "Testing Methods"
+
+    @testset "Delay" begin
+        @info "Testing Delay"
+        
+        s0 = sin.((0:0.01:6pi))
+        s1 = s0[1:end-1]
+        s2 = s0[2:end]
+        
+        method = XcorrDelay()
+        @test compute_delay(method, s1, s1) == 0
+        @test compute_delay(method, s1, s2) == 1
+        @test compute_delay(method, s2, s1) == -1
+        
+        method = DTWDelay()
+        @test compute_delay(method, s1, s1) == 0
+        @test compute_delay(method, s1, s2) == 1
+        @test compute_delay(method, s2, s1) == -1
+        
+        method = Delay(delay_method = XcorrDelay(), master = 1)
+        inds = compute_aligning_indices([s1,s2], method)
+        @test inds == [1:length(s1)-1, 2:length(s2)]
+
+        method = Delay(delay_method = DTWDelay(), master = 1)
+        inds = compute_aligning_indices([s1,s2], method)
+        @test inds == [1:length(s1)-1, 2:length(s2)]
+    end
+
+
+    @testset "Warp" begin
+        @info "Testing Warp"
+        method = Warp(warp_method=DTW(radius=3), master=1)
+        inds = compute_aligning_indices([s1,s2], method)
+        @test inds[1] == 1:length(s1)
+        @test inds[2] == [1; 1:length(s2)-1]
+
+        inds = compute_aligning_indices(signals, method)
+        @test all(length.(inds) .>= length(inds[1])) # all should be at least as long as the master
+    end
+end
+
+
+    @testset "align_signals" begin
+        @info "Testing align_signals"
+        method = Delay(delay_method=DTWDelay(), master=1)
+        for method ∈ [
+                Delay(delay_method = DTWDelay(), master=1)
+                Delay(delay_method = XcorrDelay(), master = 1)
+            ]
+            @show method
+            # test with same length
+            inds = align_signals(signals, method)
+            aligned = getindex.(signals, inds)
+            @test all(length.(inds) .== length(inds[1]))
+            @test all(norm.(aligned .- (aligned[1], )) .< (method.delay_method isa DTWDelay ? 0.5 : 2.5))
+            
+            # test with different lengths
+            inds = align_signals(signals_unequal, method)
+            aligned = getindex.(signals_unequal, inds)
+            @test all(length.(inds) .== length(inds[1]))
+            @test all(norm.(aligned .- (aligned[1], )) .< (method.delay_method isa DTWDelay ? 0.5 : 5))
+            
+            # plot(signals, layout=2, sp=1)
+            # plot!(aligned, sp=2)
+            # display(current())
+        end
+        
+        method = Delay(master=1)
+        inds = align_signals(signals_short, method; output=Indices())
+        sa = getindex.(signals_short, inds)
+        sa2 = align_signals(signals_short, method; output=Signals())
+        @test all(s1 == s2 for (s1,s2) in zip(sa, sa2))
+            
+        inds = align_signals(signals_mv, method; output=Indices())
+        @test all(reduce(vcat, inds) .<= 10)
+        sa = align_signals(signals_mv, method; output=Signals())
+        @test all(size.(sa) .== Ref((2, length(inds[1]))))
+        
+        @test all(length.(inds) .== length(inds[1]))
+
+    end
 end
